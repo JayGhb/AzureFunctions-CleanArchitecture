f4b71dcdb5a7787d51238266522784b72bf14003
JayGhb <manoloui@outlook.com.gr>
2023-07-03 00:55:32 +0300

    Update unit tests regarding latest changes

--- tests/Appliaction.UnitTests/GetSlotsQueryHandlerUnitTests.cs
+++ tests/Appliaction.UnitTests/GetSlotsQueryHandlerUnitTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Logging;
 using Moq;
 using SlottingMock.Application.Common.Interfaces;
 using SlottingMock.Application.UseCases.Queries.GetSlots;
@@ -7,22 +8,26 @@ namespace Appliaction.UnitTests
     public class GetSlotsQueryHandlerUnitTests
     {
         private readonly Mock<IDynamicsCrmService> _ruleEngineServiceMock;
+        private readonly Mock<ILogger<GetSlotsQueryHandler>> _loggerMock;
 
         public GetSlotsQueryHandlerUnitTests()
         {
             _ruleEngineServiceMock = new Mock<IDynamicsCrmService>();
+            _loggerMock = new Mock<ILogger<GetSlotsQueryHandler>>();
         }
 
         [Fact]
         public async void Handler_must_return_empty_string()
         {
             //Arrange
-            _ruleEngineServiceMock.Setup(x => x.GetDataAsync()).Returns(new List<string>() { "I am not empty" });
+            _ruleEngineServiceMock
+                .Setup(x => x.GetDataAsync(default).Result)
+                .Returns(new List<string>() { "I am not empty" });
             GetSlotsQuery fakeQuery = new GetSlotsQuery("1999-05-25");
             string expectedResult = string.Empty;
 
             //Act
-            GetSlotsQueryHandler fakeHandler = new GetSlotsQueryHandler(_ruleEngineServiceMock.Object);
+            GetSlotsQueryHandler fakeHandler = new GetSlotsQueryHandler(_ruleEngineServiceMock.Object, _loggerMock.Object);
             string actualResult = await fakeHandler.Handle(fakeQuery, default);
 
             //Assert
